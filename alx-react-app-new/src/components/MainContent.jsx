54506da1e6d5194add077d71c2fe9f5aa8a42d2f
Omorowa Edwin <edwinomorowa33@gmail.com>
2025-11-14 16:52:44 +0100

    the new code

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: '20px',
+        backgroundColor: '#eef',
+        margin: '10px',
+        borderRadius: '8px'
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
