4ec607a8fa85ae6992144fb252d9e99b0fec1c60
Omorowa Edwin <edwinomorowa33@gmail.com>
2025-11-23 15:10:27 +0100

    fix recipeStore: add updateRecipe and deleteRecipe for ALX

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,11 +1,25 @@
-import { create } from 'zustand';
+// src/components/recipeStore.js
+import { create } from "zustand";
 
-const useRecipeStore = create((set) => ({
+export const useRecipeStore = create((set) => ({
+  // State
   recipes: [],
+
+  // Actions
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+    })),
+
   setRecipes: (recipes) => set({ recipes }),
 }));
-
-export default useRecipeStore;
-
