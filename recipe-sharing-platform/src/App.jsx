ce6af0da1edea72d3c562b40b6a2ac87d0d6ba14
Omorowa Edwin <edwinomorowa33@gmail.com>
2025-12-06 11:33:19 +0100

    setting up the CSS

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import HomePage from "./components/HomePage";
-import RecipeDetail from "./components/RecipeDetail"; // create this later
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   return (
